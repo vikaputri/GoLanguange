386b66e7c910acedaf35f49d3516112e95c9aee8
vikaputri <vikaputri909@gmail.com>
2022-08-06 21:56:30 +0700

    database

---
+++ Lanjut/database/sql_test.go
@@ -0,0 +1,49 @@
+package belajar_golang_database
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(id, name) VALUES('hanif', 'Hanif')"
+	_, err := db.ExecContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new customer")
+
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name  FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id : ", id)
+		fmt.Println("Name : ", name)
+	}
+	defer rows.Close()
+
+}
