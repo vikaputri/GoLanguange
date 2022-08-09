e13229bce810d6ad10f4d9ef07d4a21aeff078ac
vikaputri <vikaputri909@gmail.com>
2022-08-09 16:48:35 +0700

    web

---
+++ Lanjut/web/http_test.go
@@ -0,0 +1,26 @@
+package web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func HelloHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Hello World")
+}
+
+func TestHTTP(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+	fmt.Println(bodyString)
+
+}
